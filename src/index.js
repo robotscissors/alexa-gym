d435fa73b5dcc574990ac82c378ef13efd83c0fb
robotscissors <cjbazin@gmail.com>
2017-07-11 22:38:46 -0700

    complete structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 "use strict";
-
+var APP_ID = "amzn1.ask.skill.5091e1a5-fb91-45aa-bf14-5b6dc4dc29a7";
 var Alexa = require('alexa-sdk');
 
 exports.handler = function(event, context, callback){
     var alexa = Alexa.handler(event, context, callback);
+    alexa.appId = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
@@ -11,7 +12,7 @@ exports.handler = function(event, context, callback){
 //handlers with intent
 var handlers = {
     'LaunchRequest': function () {
-        this.emit('Welcome Intent');
+        this.emit(':tell', 'Started Gym Application');
     },
 
     'WelcomeIntent': function () {
@@ -46,5 +47,13 @@ var handlers = {
     'GetNumberOfVisitsSinceDateIntent': function () {
       //do something amazing error capture?
         this.emit(':tell', 'You haven\'t been to the gym since..');
-    }
+    },
+
+    'AMAZON.HelpIntent': function () {
+        this.emit(':tell', 'Just tell me you are going to the gym and I will make note of it. If you want to know how many times you have been to them, just ask ... how many times have I been to the gym?');
+    },
+
+    'Unhandled': function () {
+        this.emit(':tell', 'You are having trouble.');
+    },
  };
